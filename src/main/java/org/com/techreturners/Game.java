175b14bba68bc43156d8701a7fa11c85e13bdf3c
Saranya Govind <saranya789@gmail.com>
2023-02-11 22:14:36 +0000

    isSpare boolean function added

--- src/main/java/org/com/techreturners/Game.java
+++ src/main/java/org/com/techreturners/Game.java
@@ -6,13 +6,32 @@ public class Game {
     private int rolls[] = new int[21];
     private int currentRoll = 0;
 public void roll(int pins){
-    score += pins;
+    rolls[currentRoll++] = pins;
 }
 
 public int score(){
     int score = 0;
     int frameIndex = 0;
+for (int frame = 0; frame< 10; frame ++){
+  //  if(rolls[frameIndex] + rolls[frameIndex+1] == 10){
+    if(isSpare(frameIndex)){
+        score += 10 + rolls[frameIndex +2];
+        frameIndex += 2;
+    } else{
+        score += rolls[frameIndex] + rolls[frameIndex + 1];
+        frameIndex +=2;
+    }
 
+}
     return score;
 }
+
+private boolean isSpare(int frameIndex){
+    return rolls[frameIndex] +rolls[frameIndex+1] ==10;
+}
+
+
+
+
+
 }
